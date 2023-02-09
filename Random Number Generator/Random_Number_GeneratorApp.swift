7a51eb2a9df12aa26832bfcc73df56161b6e53d6
Michael Ebimomi <mebimomi.projects@gmail.co>
2023-02-09 13:31:38 +0000

    Initial Commit

---
+++ Random Number Generator/Random_Number_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Number_GeneratorApp.swift
+//  Random Number Generator
+//
+//  Created by Michael Ebimomi on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Random_Number_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
